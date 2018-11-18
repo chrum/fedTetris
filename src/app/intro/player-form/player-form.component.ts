f8b3240f8ad2708b2a2ba943f9513897884740de
chrystian <chrystianr@gmail.com>
2018-11-18 18:14:49 +0100

    feat(intro): Moving app welcome to intro/playerForm

--- src/app/intro/player-form/player-form.component.ts
+++ src/app/intro/player-form/player-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-player-form',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-form.component.scss']
 })
 export class PlayerFormComponent implements OnInit {
-
+  @Input() gameTitle = 'no name';
   constructor() { }
 
   ngOnInit() {
