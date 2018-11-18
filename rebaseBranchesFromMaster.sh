e99125508dec1c824d446594091d0fdc9f69af3c
chrystian <chrystianr@gmail.com>
2018-11-18 18:43:57 +0100

    chore(): Adding mass rebase script

---
+++ rebaseBranchesFromMaster.sh
@@ -0,0 +1,3 @@
+for branch in 01.intro-form 02.TASK-main-game 03.game-start-btn 04.TASK-game-end-btn 05.intro-form-input 06.TASK-game-input 07.form-submitted-event 08.TASK-game-ended-event 09.name-input-element 10.TASK-email-input-element 11.form-element 12.tetris-core 13.controller 14.hotkeys 15.hiscore 16.TASK-my-score 17.score-service 18.score-submit;
+    do git rebase master $branch;
+done
