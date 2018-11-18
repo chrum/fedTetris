7050de0aaa60ad8e3089d4e6e918807035db6f63
chrystian <chrystianr@gmail.com>
2018-11-18 18:29:26 +0100

    feat(game): adding ended event/output

--- src/app/main/game/game.component.ts
+++ src/app/main/game/game.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +7,14 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class GameComponent implements OnInit {
   @Input() playerName = 'unnamed';
+  @Output() ended = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  end() {
+    this.ended.emit();
   }
 
 }
