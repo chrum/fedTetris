5d44f703204f866d6c87647d26b6093d1b1d55e1
chrystian <chrystianr@gmail.com>
2018-11-18 19:25:22 +0100

    feat(game): adding and wiring game controller

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameComponent } from './game/game.component';
 import {TetrisCoreModule} from 'ngx-tetris';
+import { ControllerComponent } from './controller/controller.component';
 
 @NgModule({
-  declarations: [GameComponent],
+  declarations: [GameComponent, ControllerComponent],
   imports: [
     CommonModule,
     TetrisCoreModule
